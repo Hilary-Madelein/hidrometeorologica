e5a0f7fbf7ab8b55eecd7dbd125ee9d25b86588d
Hilary-Madelein <madeleycc15@gamil.com>
2024-09-17 10:39:38 -0500

    Incorporacion conexion, cambios en vista

--- src/fragments/Filtro.jsx
+++ src/fragments/Filtro.jsx
@@ -12,7 +12,7 @@ function Filtro() {
                     <div className="d-flex align-items-center mb-2" style={{ justifyContent: 'flex-start' }}>
 
                         <label className="mb-2" htmlFor="fecha-inicio" style={{ fontWeight: 'bold', color: "var(--blue-unl)", marginLeft: '5px' }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-map" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pin-map" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}> 
                                 <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z" />
                                 <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z" />
                             </svg>
@@ -30,7 +30,7 @@ function Filtro() {
                     <div className="d-flex align-items-center mb-2" style={{ justifyContent: 'flex-start' }}>
 
                         <label className="mb-2" htmlFor="fecha-inicio" style={{ fontWeight: 'bold', color: "var(--blue-unl)", marginLeft: '5px' }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}>
                                 <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0" />
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                             </svg>
@@ -49,7 +49,7 @@ function Filtro() {
                     <div className="d-flex align-items-center mb-2" style={{ justifyContent: 'flex-start' }}>
 
                         <label className="mb-2" htmlFor="fecha-fin" style={{ fontWeight: 'bold', color: "var(--blue-unl)", marginLeft: '5px' }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16" style={{ marginRight: '5px', color: "var(--blue-unl)" }}>
                                 <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0" />
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                             </svg>
---
+++ src/hooks/Conexion.js
@@ -0,0 +1,104 @@
+const URL = 'http://localhost:3006/api';
+export const URLBASE = "http://localhost:3006"; 
+
+export const InicioSesion = async (data) => {
+    const headers = {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json'
+    };
+    const datos = await (await fetch(URL + "/sesion", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data)
+        
+    })).json();
+    return datos;
+}
+
+
+
+export const ObtenerGet = async (key, url) => {
+    const headers = {
+        "Content-Type": "application/json",
+        "X-API-TOKEN": key
+    };
+    const datos = await (await fetch(`${URL}/${url}`, {
+        method: "GET",
+        headers: headers,
+    })).json();
+    return datos;
+}
+
+export const ObtenerPost = async (key, url, bodyData) => {
+    const headers = {
+        "Content-Type": "application/json",
+        "X-API-TOKEN": key
+    };
+
+    try {
+        const response = await fetch(`${URL}/${url}`, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(bodyData)
+        });
+
+        const text = await response.text();
+        let datos;
+
+        try {
+            datos = JSON.parse(text);
+        } catch (error) {
+            throw new Error(`La respuesta no es un JSON válido: ${text}`);
+        }
+        return datos;
+
+    } catch (error) {
+        console.error("Error al realizar la solicitud POST:", error);
+        throw error;
+    }
+}
+
+
+
+export const Obtener = async (id, key, url) => {
+    var cabeceras = { "X-API-TOKEN": key };
+    const datos = await (await fetch(`${URL}${url}${id}`, {
+        method: "GET",
+        headers: cabeceras
+    })).json();
+    return datos;
+} 
+
+export const PostGuardar = async (data, key, urls) => {
+    const headers = {
+        "Content-Type": "application/json",
+        "X-API-TOKEN": key
+    };
+    const datos = await (await fetch(`${URL}/${urls}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+    })).json();
+    return datos;
+}
+
+export const GuardarImages = async (data, key, urls) => {
+    const headers = {
+        "x-api-token": key,
+    };
+    const requestOptions = {
+        method: "POST",
+        headers: headers,
+        body: data,
+    };
+    try {
+        const response = await fetch(URL + urls, requestOptions);
+
+        const datos = await response.json();
+
+        return datos;
+    } catch (error) {
+        console.log("Error:", error);
+        throw error;
+    }
+}
